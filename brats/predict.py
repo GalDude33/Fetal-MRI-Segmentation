e3d8cbe13da4bfb898034625150552f3c12e34e8
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-08-04 16:15:57 +0300

    Added support to predict for both models

--- brats/predict.py
+++ brats/predict.py
@@ -1,10 +1,9 @@
 import os
 
-from train import config
 from unet3d.prediction import run_validation_cases
+import argparse
 
-
-def main():
+def main(config):
     prediction_dir = os.path.abspath("prediction")
     run_validation_cases(validation_keys_file=config["validation_file"],
                          model_file=config["model_file"],
@@ -16,4 +15,17 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Choose evaluation model.')
+    parser.add_argument('--model', type=str, default='isensee2017', required=False,
+                        help='Choose model [unet3d, isensee2017]')
+    args = parser.parse_args()
+    if args.model == 'unet3d':
+        import train
+        config = train.config
+    elif args.model == 'isensee2017':
+        import train_isensee2017
+        config = train_isensee2017.config
+    else:
+        raise Exception('Unknown model')
+
+    main(config)
