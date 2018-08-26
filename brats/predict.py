aea65a03295b0e662b9a4f19391dbc5d8ff8eaa6
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-08-26 21:20:49 +0300

    Added option to choose split and added test split support for prediction. also modified saved prediction dir

--- brats/predict.py
+++ brats/predict.py
@@ -5,9 +5,11 @@ from fetal_net.prediction import run_validation_cases
 import argparse
 
 
-def main(config, overlap_factor=1):
-    prediction_dir = os.path.abspath(config['base_dir'])
-    run_validation_cases(validation_keys_file=config["validation_file"],
+def main(config, split='test', overlap_factor=1):
+    prediction_dir = os.path.abspath(os.path.join(config['base_dir'], 'predictions', split))
+
+    indices_file = config["test_file"] if split == 'test' else config["validation_file"]
+    run_validation_cases(validation_keys_file=indices_file,
                          model_file=config["model_file"],
                          training_modalities=config["training_modalities"],
                          labels=config["labels"],
@@ -23,6 +25,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_dir", help="specifies config dir path",
                         type=str, required=True)
+    parser.add_argument("--split", help="What split to predict on? (test/val)",
+                        type=str, default='test')
     parser.add_argument("--overlap_factor", help="specifies overlap between prediction patches",
                         type=int, default=1)
     opts = parser.parse_args()
@@ -30,4 +34,4 @@ if __name__ == "__main__":
     with open(os.path.join(opts.config_dir, 'config.json')) as f:
         config = json.load(f)
 
-    main(config, opts.overlap_factor)
+    main(config, opts.split, opts.overlap_factor)
