64d0ca6c396f06e28b6a555b728b5d54a26bfeee
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-08-28 01:03:41 +0300

    Added isensee import in init

--- fetal_net/model/__init__.py
+++ fetal_net/model/__init__.py
@@ -4,3 +4,4 @@ from .fetal_net_skip2 import fetal_origin2_model
 from .fetal_net_skip3 import fetal_origin3_model
 
 from .unet.unet import unet_model_2d
+from .unet.isensee import isensee2017_model
