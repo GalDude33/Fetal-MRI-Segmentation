70ca161689d3c02052d908d83eae15d970cdcccb
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-08-28 01:53:20 +0300

    added accuracy metric to isensee

--- fetal_net/model/unet/isensee.py
+++ fetal_net/model/unet/isensee.py
@@ -80,7 +80,7 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
     activation_block = Activation(activation_name)(output_layer)
     activation_block = Permute((2, 3, 1))(activation_block)
     model = Model(inputs=inputs, outputs=activation_block)
-    model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
+    model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function, metrics=['acc'])
     return model
 
 
