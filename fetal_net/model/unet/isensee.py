18bbcb2a6c227b0282388d013c6a76b1423e408d
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-08-28 02:42:29 +0300

    .

--- fetal_net/model/unet/isensee.py
+++ fetal_net/model/unet/isensee.py
@@ -5,7 +5,7 @@ from keras.engine import Model
 from keras.optimizers import Adam
 
 from .unet import create_convolution_block, concatenate
-from ...metrics import weighted_dice_coefficient_loss
+from ...metrics import weighted_dice_coefficient_loss, dice_coefficient_loss
 
 import keras.backend as K
 
@@ -19,7 +19,7 @@ create_convolution_block = partial(create_convolution_block, activation=LeakyReL
 
 def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5, dropout_rate=0.3,
                       n_segmentation_levels=3, n_labels=1, optimizer=Adam, initial_learning_rate=5e-4,
-                      loss_function=weighted_dice_coefficient_loss, activation_name="sigmoid"):
+                      loss_function=dice_coefficient_loss, activation_name="sigmoid"):
     """
     This function builds a model proposed by Isensee et al. for the BRATS 2017 competition:
     https://www.cbica.upenn.edu/sbia/Spyridon.Bakas/MICCAI_BraTS/MICCAI_BraTS_2017_proceedings_shortPapers.pdf
