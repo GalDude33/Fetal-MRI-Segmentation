682793a7ae5caa7a67379fb6c25cc460ff31d75b
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2018-10-30 18:56:03 +0200

    Added norm net model

--- fetal_net/model/__init__.py
+++ fetal_net/model/__init__.py
@@ -8,3 +8,5 @@ from .unet.isensee import isensee2017_model
 
 from .unet3d.unet import unet_model_3d
 from .unet3d.isensee2017 import isensee2017_model_3d
+
+from .norm.NormNet import norm_net_model
