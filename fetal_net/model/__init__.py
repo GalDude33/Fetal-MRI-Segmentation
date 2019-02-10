afbecb21b68795eab7e3f72ac79fbc8e450373bc
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2019-02-10 15:18:51 +0200

    Added 2d discriminator

--- fetal_net/model/__init__.py
+++ fetal_net/model/__init__.py
@@ -10,8 +10,9 @@ from .unet.isensee import isensee2017_model
 
 from .unet3d.unet import unet_model_3d
 from .unet3d.isensee2017 import isensee2017_model_3d
-from .unet3d.isensee2017_2 import isensee2017_model_3d as isensee2017_2_model_3d
+#from .unet3d.isensee2017_2 import isensee2017_model_3d as isensee2017_2_model_3d
 
 from .norm.NormNet import norm_net_model
 
-from .discriminator.all_dis import discriminator_image
+from .discriminator.all_dis_2d import discriminator_image_2d
+from .discriminator.all_dis_3d import discriminator_image_3d
