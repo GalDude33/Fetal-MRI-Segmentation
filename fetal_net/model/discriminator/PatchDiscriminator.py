09b0bd29b69470fa8ae50186fc8a68311690610e
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2019-03-08 16:10:47 +0200

    Yet another model

--- fetal_net/model/discriminator/PatchDiscriminator.py
+++ fetal_net/model/discriminator/PatchDiscriminator.py
@@ -1,29 +1,29 @@
 from keras import Input, Model
-from keras.layers import LeakyReLU, Conv2D, Conv3D
+from keras.layers import LeakyReLU, Conv2D, Conv3D, Activation
 from keras_contrib.layers import InstanceNormalization
 
 
-def build_discriminator(input_shape, df=64, conv=None):
-    if conv is None:
-        raise AssertionError("conv should be on of {Conv2D, Conv3D}")
+def d_layer(layer_input, filters, conv, strides=(2,2,2), f_size=4, normalization=True):
+    """Discriminator layer"""
+    d = conv(filters, kernel_size=f_size, strides=strides, padding='same')(layer_input)
+    d = LeakyReLU(alpha=0.2)(d)
+    if normalization:
+        d = InstanceNormalization(axis=-1)(d)
+    return d
 
-    def d_layer(layer_input, filters, f_size=4, normalization=True):
-        """Discriminator layer"""
-        d = conv(filters, kernel_size=f_size, strides=(2,2,1), padding='same')(layer_input)
-        d = LeakyReLU(alpha=0.2)(d)
-        if normalization:
-            d = InstanceNormalization()(d)
-        return d
+
+def build_discriminator(input_shape, df=64, conv=None, depth=4, only_xy_pool=2):
+    if conv is None:
+        raise AssertionError("conv should be one of {Conv2D, Conv3D}")
 
     img = Input(shape=input_shape)
 
-    d1 = d_layer(img, df, normalization=False)
-    d2 = d_layer(d1, df * 2)
-    d3 = d_layer(d2, df * 4)
-    d4 = d_layer(d3, df * 8)
+    d1 = img
+    for i in range(0, depth):
+        pool_size = (2, 2, 1) if i < only_xy_pool else (2, 2, 2)
+        d1 = d_layer(d1, df * (2 ** i), conv, strides=pool_size, normalization=(i>0))
 
-    validity = conv(1, kernel_size=4, strides=1, padding='same', activation='sigmoid')(d4)
-
+    validity = conv(1, kernel_size=4, strides=1, padding='same', activation='sigmoid')(d1)
     return Model(img, validity)
 
 
@@ -31,5 +31,5 @@ def build_discriminator_2d(input_shape, df=64, **kwargs):
     return build_discriminator(input_shape, df=df, conv=Conv2D)
 
 
-def build_discriminator_3d(input_shape, df=32, **kwargs):
-    return build_discriminator(input_shape, df=df, conv=Conv3D)
+def build_discriminator_3d(input_shape, df=32, depth=4, only_xy_layers=2, **kwargs):
+    return build_discriminator(input_shape, df=df, conv=Conv3D, depth=4, only_xy_pool=2)
