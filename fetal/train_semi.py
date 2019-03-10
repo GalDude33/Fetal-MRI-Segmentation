d445cf6607dbc9e9d122038a8288c0b0aa2d6442
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2019-03-10 02:51:10 +0200

    Fixed and updated

--- fetal/train_semi.py
+++ fetal/train_semi.py
@@ -5,7 +5,7 @@ import keras.backend as K
 import numpy as np
 from keras import Input, Model
 from keras.engine.network import Network
-from keras.layers import Activation, Concatenate
+from keras.layers import Activation, Concatenate, Lambda
 from keras.optimizers import Adam
 from tqdm import tqdm
 
@@ -13,9 +13,10 @@ import fetal_net
 import fetal_net.metrics
 import fetal_net.preprocess
 from fetal.config_utils import get_config
-from fetal.utils import get_last_model_path, create_data_file, set_gpu_mem_growth
+from fetal.utils import get_last_model_path, create_data_file, set_gpu_mem_growth, Scheduler, build_dsc
 from fetal_net.data import open_data_file
 from fetal_net.generator import get_training_and_validation_generators
+from fetal_net.model.discriminator import PatchDiscriminator
 from fetal_net.model.fetal_net import fetal_envelope_model
 
 set_gpu_mem_growth()
@@ -31,53 +32,8 @@ if not "dis_steps" in config:
     config["dis_steps"] = 1
 if not "gd_loss_ratio" in config:
     config["gd_loss_ratio"] = 10
-
-
-class Scheduler:
-    def __init__(self, n_itrs_per_epoch_d, n_itrs_per_epoch_g, init_lr, lr_decay, lr_patience):
-        self.init_dsteps = n_itrs_per_epoch_d
-        self.init_gsteps = n_itrs_per_epoch_g
-        self.init_lr = init_lr
-        self.lr_decay = lr_decay
-        self.lr_patience = lr_patience
-
-        self.dsteps = self.init_dsteps
-        self.gsteps = self.init_gsteps
-        self.lr = self.init_lr
-        self.steps_stuck = 0
-        self.best_loss = np.inf
-
-    def get_dsteps(self):
-        return self.dsteps
-
-    def get_gsteps(self):
-        return self.gsteps
-
-    def get_lr(self):
-        return self.lr
-
-    def update_steps(self, n_round, loss):
-        if loss < self.best_loss:
-            self.steps_stuck = 0
-            self.best_loss = loss
-        else:
-            self.steps_stuck += 1
-
-        if self.steps_stuck > self.lr_patience:
-            self.lr *= self.lr_decay
-            self.steps_stuck = 0
-            print('Reducing LR to {}'.format(self.lr))
-
-        # if key in self.schedules['step_decay']:
-        # self.dsteps = max(int(self.init_dsteps * self.schedules['step_decay'][key]), 1)
-        # self.gsteps = max(int(self.init_gsteps * self.schedules['step_decay'][key]), 1)
-
-
-def build_dsc(out_labels, outs):
-    s = ''
-    for l, o in zip(out_labels, outs):
-        s = s + '{}={:.3f}, '.format(l, o)
-    return s[:-2] + '|'
+if not "merge_method" in config:
+    config["merge_method"] = 'mul_merge'  #'mul_merge'
 
 
 def add_noise_to_segs(segs):
@@ -92,21 +48,31 @@ def add_noise_to_segs(segs):
 def mul_merge_maps(r, s):
     r_plus = r * s
     r_minus = r * (1 - s)
-    return np.concatenate((r_plus, r_minus), axis=1)
+    return np.concatenate((r_plus, r_minus), axis=-1)
 
 
-def input2discriminator(real_patches, real_segs, semi_patches, semi_segs, d_out_shape, mul_merge=True):
+def input2discriminator(real_patches, real_segs,
+                        semi_patches, semi_segs,
+                        d_out_shape, mul_merge=config['merge_method'], add_noise=False):
+    if not config["3D"]:
+        t_i = config["truth_index"]
+        real_patches = real_patches[..., t_i:t_i+1]
+        semi_patches = semi_patches[..., t_i:t_i+1]
+
+    if add_noise:
+        real_segs = add_noise_to_segs(real_segs)
+
     if mul_merge:
-        real = mul_merge_maps(real_patches, add_noise_to_segs(real_segs))
+        real = mul_merge_maps(real_patches, real_segs)
         fake = mul_merge_maps(semi_patches, semi_segs)
     else:
-        real = np.concatenate((real_patches, add_noise_to_segs(real_segs)), axis=1)
-        fake = np.concatenate((semi_patches, semi_segs), axis=1)
+        real = np.concatenate((real_patches, real_segs), axis=-1)
+        fake = np.concatenate((semi_patches, semi_segs), axis=-1)
 
     d_x_batch = np.concatenate((real, fake), axis=0)
 
     # real : 1, fake : 0
-    d_y_batch = np.clip(np.random.uniform(0.9, 1.0, size=[d_x_batch.shape[0]] + list(d_out_shape)[1:]),
+    d_y_batch = np.clip(np.random.uniform(0.85, 0.95, size=[d_x_batch.shape[0]] + list(d_out_shape)[1:]),
                         a_min=0, a_max=1)
     d_y_batch[real.shape[0]:, ...] = 1 - d_y_batch[real.shape[0]:, ...]
 
@@ -118,7 +84,10 @@ def input2gan(real_patches, real_segs, semi_patches, d_out_shape):
     # set 1 to all labels (real : 1, fake : 0)
     g_y_batch = [
         real_segs,
-        np.clip(np.random.uniform(0.9, 1.0, size=[real_patches.shape[0]] + list(d_out_shape)[1:]), a_min=0, a_max=1)
+        # Real
+        np.clip(np.random.uniform(0.85, 0.95, size=[real_patches.shape[0]] + list(d_out_shape)[1:]), a_min=0, a_max=1),
+        # Fake
+        np.clip(np.random.uniform(0.85, 0.95, size=[real_patches.shape[0]] + list(d_out_shape)[1:]), a_min=0, a_max=1)
     ]
     return g_x_batch, g_y_batch
 
@@ -134,7 +103,7 @@ def main(overwrite=False):
     dis_loss_func = getattr(fetal_net.metrics, config['dis_loss'])
 
     # instantiate new model
-    seg_model_func = getattr(fetal_net.model, config['model_name'])
+    seg_model_func = getattr(fetal_net.model, config['seg_model_name'])
     gen_model = seg_model_func(input_shape=config["input_shape"],
                                initial_learning_rate=config["initial_learning_rate"],
                                **{'dropout_rate': config['dropout_rate'],
@@ -143,23 +112,23 @@ def main(overwrite=False):
                                       "input_shape"],
                                   'old_model_path': config['old_model']})
 
-    dis_model_func = getattr(fetal_net.model, config['dis_model_name'])
-    dis_model = dis_model_func(
-        input_shape=[config["input_shape"][0] + config["n_labels"]] + config["input_shape"][1:],
-        initial_learning_rate=config["initial_learning_rate"],
-        **{'dropout_rate': config['dropout_rate'],
-           'loss_function': dis_loss_func})
-
     if not overwrite \
             and len(glob.glob(config["model_file"] + 'g_*.h5')) > 0:
-        # dis_model_path = get_last_model_path(config["model_file"] + 'dis_')
         gen_model_path = get_last_model_path(config["model_file"] + 'g_')
-        # print('Loading dis model from: {}'.format(dis_model_path))
         print('Loading gen model from: {}'.format(gen_model_path))
-        # dis_model = load_old_model(dis_model_path)
-        # gen_model = load_old_model(gen_model_path)
-        # dis_model.load_weights(dis_model_path)
         gen_model.load_weights(gen_model_path)
+
+    # dis_model_func = getattr(fetal_net.model, config['dis_model_name'])
+    # dis_model = dis_model_func(
+    #     input_shape=[config["input_shape"][0] + config["n_labels"]] + config["input_shape"][1:],
+    #     initial_learning_rate=config["initial_learning_rate"],
+    #     **{'dropout_rate': config['dropout_rate'],
+    #        'loss_function': dis_loss_func})
+    dis_model = PatchDiscriminator.build_discriminator_2d(gen_model.output_shape[1:-1]+(2,))
+    dis_model.compile(
+        loss=['mse'],
+        metrics=['mae'],
+        optimizer=Adam(config["initial_learning_rate"]))
 
     gen_model.summary()
     dis_model.summary()
@@ -174,69 +143,68 @@ def main(overwrite=False):
 
     inputs_real = Input(shape=config["input_shape"])
     inputs_fake = Input(shape=config["input_shape"])
-    segs_real = Activation(None, name='seg_real')(gen_model(inputs_real))
-    segs_fake = Activation(None, name='seg_fake')(gen_model(inputs_fake))
-    valid = Activation(None, name='dis')(frozen_dis_model(Concatenate(axis=1)([segs_fake, inputs_fake])))
+    segs_real = Activation(None, name='s_R')(gen_model(inputs_real))
+    segs_fake = Activation(None, name='s_S')(gen_model(inputs_fake))
+
+    def mul_merge_k(seg, inp):
+        return Lambda(lambda s_i: K.concatenate([s_i[0] * s_i[1],
+                                                 (1-s_i[0]) * s_i[1]]))([seg, inp])
+
+    def slice_channel_k(t, i):
+        return Lambda(lambda t: t[..., i:i+1])(t)
+
+    if config["merge_method"] == 'mul_merge':
+        real_dis_input = mul_merge_k(segs_real, slice_channel_k(inputs_real, config["truth_index"]))
+        semi_dis_input = mul_merge_k(segs_fake, slice_channel_k(inputs_fake, config["truth_index"]))
+    else:
+        real_dis_input = Concatenate()([segs_real, slice_channel_k(inputs_real, config["truth_index"])])
+        semi_dis_input = Concatenate()([segs_fake, slice_channel_k(inputs_fake, config["truth_index"])])
+
+    real_valid = Activation(None, name='dR')(frozen_dis_model(real_dis_input))
+    semi_valid = Activation(None, name='dS')(frozen_dis_model(semi_dis_input))
+
     combined_model = Model(inputs=[inputs_real, inputs_fake],
-                           outputs=[segs_real, valid])
-    combined_model.compile(loss=[seg_loss_func, 'binary_crossentropy'],
-                           loss_weights=[1, config["gd_loss_ratio"]],
+                           outputs=[segs_real, real_valid, semi_valid])
+    combined_model.compile(loss=[seg_loss_func, 'mse', 'mse'],
+                           loss_weights=[1, config["gd_loss_ratio"], config["gd_loss_ratio"]],
+                           metrics=['mae'],
                            optimizer=Adam(config["initial_learning_rate"]))
     combined_model.summary()
 
+    gen_params = dict(batch_size=config["batch_size"],
+                      data_split=config["validation_split"],
+                      overwrite=overwrite,
+                      validation_keys_file=config["validation_file"],
+                      training_keys_file=config["training_file"],
+                      test_keys_file=config["test_file"],
+                      n_labels=config["n_labels"],
+                      labels=config["labels"],
+                      patch_shape=(*config["patch_shape"], config["patch_depth"]),
+                      validation_batch_size=config["validation_batch_size"],
+                      skip_blank_train=config["skip_blank_train"],
+                      skip_blank_val=config["skip_blank_val"],
+                      truth_index=config["truth_index"],
+                      truth_size=config["truth_size"],
+                      prev_truth_index=config["prev_truth_index"],
+                      prev_truth_size=config["prev_truth_size"],
+                      truth_downsample=config["truth_downsample"],
+                      truth_crop=config["truth_crop"],
+                      patches_per_epoch=config["patches_per_epoch"],
+                      categorical=config["categorical"], is3d=config["3D"],
+                      drop_easy_patches_train=config["drop_easy_patches_train"],
+                      drop_easy_patches_val=config["drop_easy_patches_val"])
     # get training and testing generators
     train_generator, validation_generator, n_train_steps, n_validation_steps = get_training_and_validation_generators(
         data_file_opened,
-        batch_size=config["batch_size"],
-        data_split=config["validation_split"],
-        overwrite=overwrite,
-        validation_keys_file=config["validation_file"],
-        training_keys_file=config["training_file"],
-        test_keys_file=config["test_file"],
-        n_labels=config["n_labels"],
-        labels=config["labels"],
-        patch_shape=(*config["patch_shape"], config["patch_depth"]),
-        validation_batch_size=config["validation_batch_size"],
         augment=config["augment"],
-        skip_blank_train=config["skip_blank_train"],
-        skip_blank_val=config["skip_blank_val"],
-        truth_index=config["truth_index"],
-        truth_size=config["truth_size"],
-        prev_truth_index=config["prev_truth_index"],
-        prev_truth_size=config["prev_truth_size"],
-        truth_downsample=config["truth_downsample"],
-        truth_crop=config["truth_crop"],
-        patches_per_epoch=config["patches_per_epoch"],
-        categorical=config["categorical"], is3d=config["3D"],
-        drop_easy_patches_train=config["drop_easy_patches_train"],
-        drop_easy_patches_val=config["drop_easy_patches_val"])
+        **gen_params
+    )
 
     # get training and testing generators
     _, semi_generator, _, _ = get_training_and_validation_generators(
         data_file_opened,
-        batch_size=config["batch_size"],
-        data_split=config["validation_split"],
-        overwrite=overwrite,
-        validation_keys_file=config["validation_file"],
-        training_keys_file=config["training_file"],
-        test_keys_file=config["test_file"],
-        n_labels=config["n_labels"],
-        labels=config["labels"],
-        patch_shape=(*config["patch_shape"], config["patch_depth"]),
-        validation_batch_size=config["validation_batch_size"],
         val_augment=config["augment"],
-        skip_blank_train=config["skip_blank_train"],
-        skip_blank_val=config["skip_blank_val"],
-        truth_index=config["truth_index"],
-        truth_size=config["truth_size"],
-        prev_truth_index=config["prev_truth_index"],
-        prev_truth_size=config["prev_truth_size"],
-        truth_downsample=config["truth_downsample"],
-        truth_crop=config["truth_crop"],
-        patches_per_epoch=config["patches_per_epoch"],
-        categorical=config["categorical"], is3d=config["3D"],
-        drop_easy_patches_train=config["drop_easy_patches_train"],
-        drop_easy_patches_val=config["drop_easy_patches_val"])
+        **gen_params)
 
     # start training
     scheduler = Scheduler(config["dis_steps"], config["gen_steps"],
@@ -253,9 +221,12 @@ def main(overwrite=False):
                 # train D
                 outputs = np.zeros(dis_model.metrics_names.__len__())
                 for i in range(scheduler.get_dsteps()):
-                    real_patches, real_segs = next(train_generator)
+                    real_patches, _ = next(train_generator)
                     semi_patches, _ = next(semi_generator)
-                    d_x_batch, d_y_batch = input2discriminator(real_patches, real_segs,
+                    d_x_batch, d_y_batch = input2discriminator(real_patches,
+                                                               gen_model.predict(real_patches,
+                                                                                 batch_size=config["batch_size"]),
+                                                               # Fake:
                                                                semi_patches,
                                                                gen_model.predict(semi_patches,
                                                                                  batch_size=config["batch_size"]),
@@ -284,15 +255,19 @@ def main(overwrite=False):
             evaluation_rounds = n_validation_steps
             for n_round in range(evaluation_rounds):  # rounds_for_evaluation:
                 val_patches, val_segs = next(validation_generator)
+                semi_val_patches, _ = next(semi_generator)
 
                 # D
                 if scheduler.get_dsteps() > 0:
-                    d_x_test, d_y_test = input2discriminator(val_patches, val_segs,
-                                                             val_patches,
-                                                             gen_model.predict(val_patches,
-                                                                               batch_size=config[
-                                                                                   "validation_batch_size"]),
-                                                             dis_model.output_shape)
+                    d_x_test, d_y_test = \
+                        input2discriminator(val_patches,
+                                            gen_model.predict(val_patches,
+                                                              batch_size=config["validation_batch_size"]),
+                                            # Semi
+                                            semi_val_patches,
+                                            gen_model.predict(semi_val_patches,
+                                                              batch_size=config["validation_batch_size"]),
+                                            dis_model.output_shape)
                     dis_metrics += dis_model.evaluate(d_x_test, d_y_test, batch_size=config["validation_batch_size"],
                                                       verbose=0)
 
@@ -316,10 +291,9 @@ def main(overwrite=False):
 
             postfix['val_d'] = build_dsc(dis_model.metrics_names, dis_metrics)
             postfix['val_g'] = build_dsc(gen_model.metrics_names, gen_metrics)
-            # pbar.set_postfix(**postfix)
+
             print('val_d: ' + postfix['val_d'], end=' | ')
             print('val_g: ' + postfix['val_g'])
-            # pbar.refresh()
 
             # update step sizes, learning rates
             scheduler.update_steps(epoch, gen_metrics[0])
