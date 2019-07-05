58edb2ab34011488f31a69c7c943548dca4122d7
Gal Dudovitch <Gal.Dudovitch@Gmail.com>
2019-07-05 14:12:55 +0300

    added stats

--- scripts/reduce_nii_size.py
+++ scripts/reduce_nii_size.py
@@ -11,11 +11,14 @@ src_dir = opts.src
 ext = '.gz'
 
 for p in Path(src_dir).glob('**/prediction.nii.gz'):
+    orig_size = p.stat().st_size
+
     pred = nib.load(str(p)).get_data()
 
     pred[:] = (pred * 100).astype(int) / 100.0
 
     # entropy 1
     nib.save(pred, str(p))
+    after_size = p.stat().st_size
 
-    print(p)
+    print(p.parent, orig_size, after_size)
